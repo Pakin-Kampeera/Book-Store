fae7bf46fddc9d936922ae9b06a63d6bb53a533f
Pakin-Kampeera <armpakin@hotmail.com>
2021-11-22 14:58:51 +0700

    Update: repository unit test

---
+++ src/test/java/exam/project/library/repository/BookRepositoryTest.java
@@ -0,0 +1,91 @@
+package exam.project.library.repository;
+
+import exam.project.library.model.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class BookRepositoryTest {
+    @Mock
+    JdbcTemplate jdbcTemplate;
+
+    BookRepository bookRepository;
+
+    Book book1, book2;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        bookRepository = new BookRepository(jdbcTemplate);
+
+        book1 = new Book();
+        book1.setId(1L);
+        book1.setTitle("The matrix");
+        book1.setPrice("25.50");
+
+        book2 = new Book();
+        book2.setId(2L);
+        book2.setTitle("Apocalypse");
+        book2.setPrice("30.00");
+    }
+
+    @Test
+    void getAllBook() {
+        List<Book> bookSet = new ArrayList<>();
+        bookSet.add(book1);
+        bookSet.add(book2);
+
+        when(jdbcTemplate.query(anyString(), any(ResultSetExtractor.class))).thenReturn(bookSet);
+
+        List<Book> books = bookRepository.getAllBook();
+        assertEquals(2, books.size());
+        verify(jdbcTemplate, times(1)).query(anyString(), any(ResultSetExtractor.class));
+    }
+
+    @Test
+    void getBookById() {
+        List<Book> bookSet = new ArrayList<>();
+        bookSet.add(book1);
+
+        when(jdbcTemplate.query(anyString(), any(ResultSetExtractor.class), anyLong())).thenReturn(bookSet);
+
+        List<Book> books = bookRepository.getBookById(1L);
+        assertEquals(1, books.size());
+        verify(jdbcTemplate, times(1)).query(anyString(), any(ResultSetExtractor.class), anyLong());
+    }
+
+    @Test
+    void saveNewBook() {
+        when(jdbcTemplate.update(anyString(), anyString(), anyString())).thenReturn(1);
+        bookRepository.saveNewBook(book1);
+        assertEquals(1, bookRepository.saveNewBook(book1));
+        verify(jdbcTemplate, times(2)).update(anyString(), anyString(), anyString());
+    }
+
+    @Test
+    void updateBook() {
+        when(jdbcTemplate.update(anyString(), anyString(), anyString(), anyLong())).thenReturn(1);
+        bookRepository.updateBook(1L, book1);
+        verify(jdbcTemplate, times(1)).update(anyString(), anyString(), anyString(), anyLong());
+    }
+
+    @Test
+    void deleteBook() {
+        when(jdbcTemplate.update(anyString(), anyLong())).thenReturn(1);
+        bookRepository.deleteBook(1L);
+        verify(jdbcTemplate, times(1)).update(anyString(), anyLong());
+    }
+}
---
+++ src/test/java/exam/project/library/repository/PublisherRepositoryTest.java
@@ -0,0 +1,95 @@
+package exam.project.library.repository;
+
+import exam.project.library.model.Publisher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class PublisherRepositoryTest {
+
+    @Mock
+    JdbcTemplate jdbcTemplate;
+
+    PublisherRepository publisherRepository;
+
+    Publisher publisher1, publisher2;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        publisherRepository = new PublisherRepository(jdbcTemplate);
+
+        publisher1 = new Publisher();
+        publisher1.setId(1L);
+        publisher1.setName("London House");
+        publisher1.setStreet("1234");
+        publisher1.setCity("Manchester");
+        publisher1.setZip("2456");
+
+        publisher2 = new Publisher();
+        publisher2.setId(1L);
+        publisher2.setName("City Hall");
+        publisher2.setStreet("7432");
+        publisher2.setCity("Houston");
+        publisher2.setZip("8589");
+    }
+
+    @Test
+    void getAllPublisher() {
+        List<Publisher> publisherSet = new ArrayList<>();
+        publisherSet.add(publisher1);
+        publisherSet.add(publisher2);
+
+        when(jdbcTemplate.query(anyString(), any(ResultSetExtractor.class))).thenReturn(publisherSet);
+
+        List<Publisher> publishers = publisherRepository.getAllPublisher();
+        assertEquals(2, publishers.size());
+        verify(jdbcTemplate, times(1)).query(anyString(), any(ResultSetExtractor.class));
+    }
+
+    @Test
+    void getPublisherById() {
+        List<Publisher> publisherSet = new ArrayList<>();
+        publisherSet.add(publisher1);
+
+        when(jdbcTemplate.query(anyString(), any(ResultSetExtractor.class), anyLong())).thenReturn(publisherSet);
+
+        List<Publisher> publishers = publisherRepository.getPublisherById(1L);
+        assertEquals(1, publishers.size());
+        verify(jdbcTemplate, times(1)).query(anyString(), any(ResultSetExtractor.class), anyLong());
+    }
+
+    @Test
+    void saveNewPublisher() {
+        when(jdbcTemplate.update(anyString(), anyString(), anyString(), anyString(), anyString())).thenReturn(1);
+        publisherRepository.saveNewPublisher(publisher1);
+        assertEquals(1, publisherRepository.saveNewPublisher(publisher1));
+    }
+
+    @Test
+    void updatePublisher() {
+        when(jdbcTemplate.update(anyString(), anyString(), anyString(), anyString(), anyString(), anyLong())).thenReturn(1);
+        publisherRepository.updatePublisher(1L, publisher1);
+        verify(jdbcTemplate, times(1)).update(anyString(), anyString(), anyString(), anyString(), anyString(), anyLong());
+    }
+
+    @Test
+    void deletePublisher() {
+        when(jdbcTemplate.update(anyString(), anyLong())).thenReturn(1);
+        publisherRepository.deletePublisher(1L);
+        verify(jdbcTemplate, times(1)).update(anyString(), anyLong());
+    }
+}
