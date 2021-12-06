beb49df77a1a67e6497524f76a65c6ec001ba6fe
Pakin-Kampeera <armpakin@hotmail.com>
2021-12-06 17:25:46 +0700

    Fix: redis cache

--- src/main/java/exam/project/library/service/implementations/AuthorServiceImpl.java
+++ src/main/java/exam/project/library/service/implementations/AuthorServiceImpl.java
@@ -3,6 +3,9 @@ package exam.project.library.service.implementations;
 import exam.project.library.model.Author;
 import exam.project.library.repository.AuthorRepository;
 import exam.project.library.service.AuthorService;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,35 +19,43 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     @Override
-//    @Cacheable(value = "author")
+    @Cacheable(value = "authors")
     public List<Author> getAllAuthor() {
         return authorRepository.getAllAuthor();
     }
 
     @Override
-//    @Cacheable(value = "author", key = "#authorId")
+    @Cacheable(value = "author", key = "#authorId", unless = "#result==null")
     public List<Author> getAuthorById(Long authorId) {
         return authorRepository.getAuthorById(authorId);
     }
 
     @Override
+    @CacheEvict(value = "authors", allEntries = true)
     public int saveNewAuthor(Author author) {
         return authorRepository.saveNewAuthor(author);
     }
 
     @Override
+    @CacheEvict(value = "authors", allEntries = true)
     public void saveWriteBook(Long authorId, Long bookId) {
         authorRepository.saveWriteBook(authorId, bookId);
     }
 
     @Override
-//    @CachePut(value = "author", key = "#authorId")
+    @Caching(evict = {
+            @CacheEvict(value = "authors", allEntries = true),
+            @CacheEvict(value = "author", key = "#authorId")
+    })
     public void updateAuthor(Long authorId, Author author) {
         authorRepository.updateAuthor(authorId, author);
     }
 
     @Override
-//    @CacheEvict(value = "author", key = "#authorId")
+    @Caching(evict = {
+            @CacheEvict(value = "authors", allEntries = true),
+            @CacheEvict(value = "author", key = "#authorId")
+    })
     public void deleteAuthor(Long authorId) {
         authorRepository.deleteAuthor(authorId);
     }
